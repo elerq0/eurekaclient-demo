278a4a70294716e51050bacc03f968b8847be839
elerq <elerq0@gmail.com>
2020-09-19 22:50:57 +0200

    Initial commit

---
+++ src/test/java/com/elerq/eurekaclientdemo/EurekaclientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elerq.eurekaclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaclientDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
